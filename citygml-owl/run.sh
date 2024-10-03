a27cf1acc422db97cc99726a1d757bfb4cf21c6c
Nataliya Keberle <nataliya.keberle@ontotext.com>
2024-10-03 10:19:35 +0300

    conversion for partial model Building

--- citygml-owl/run.sh
+++ citygml-owl/run.sh
@@ -1,3 +1,8 @@
-java -jar ~/prog/ShapeChange/target/ShapeChange-3.1.1-SNAPSHOT.jar -Dfile.encoding=UTF-8 -c CityGML3.0_to_OWL_lite_ontotext2_config.xml \
-  -x '$input$' './CityGML_3.0-workspaces-documents_shapechange-export.xml' \
-  -x '$output$' './converted'
+#java -jar ~/prog/ShapeChange/target/ShapeChange-3.1.1-SNAPSHOT.jar -Dfile.encoding=UTF-8 -c CityGML3.0_to_OWL_lite_ontotext_config.xml \
+#  -x '$input$' './CityGML_3.0-workspaces-documents_shapechange-export.xml' \
+#  -x '$output$' './converted'
+
+java -jar ~/prog/ShapeChange/target/ShapeChange-3.1.1-SNAPSHOT.jar -Dfile.encoding=UTF-8 -c CityGML3.0_to_OWL_lite_ontotextBuilding_config.xml \
+  -x '$input$' './CityGML_3.0_Building_UML1.3_XMI1.0_NoADEs.xml' \
+  -x '$output$' './converted' 
+  
